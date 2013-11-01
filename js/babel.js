c4f056172e2c574cb3b24e90aa772cef3796a610
Henry Hirsch <linux@wheep.de>
2013-11-01 11:39:34 +0100

    Added licence.

--- js/babel.js
+++ js/babel.js
@@ -1,74 +1,92 @@
-    function Babel($scope) {
-	$scope.translation = "";
-	$scope.message = "";
-	$scope.configuration = new Array();
-	$scope.configuration['current_exercise'] =  '1';
+/*
+  All rights reserved 2013 Henry Hirsch
 
-	$scope.exercises = new Array(
-	    {text:'Rhenus fluvius est.', translations:new Array('The Rhine is a river.', 'Rhine is a river.')},
-	    {text:'Julia fillia est.', translations:new Array('Julia is a girl.', 'Julia is girl.')});
+  This file is part of Babel.
 
-	$scope.checkTranslation = function() {
-	    var currentExercise = $scope.configuration['current_exercise'] - 1;
-	    var translations = $scope.exercises[currentExercise].translations
-	    for (var i in translations)
-	    {
-		if(translations[i].toLowerCase() == $scope.translation.toLowerCase()){
-		    $('#message').css("background-color", "green");
-		    $('#message').html('Correct answer.');
-		    $scope.nextLesson();
-		    return "Ok";
-		}
+  Babel is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Foobar is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Babel.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+function Babel($scope) {
+    $scope.translation = "";
+    $scope.message = "";
+    $scope.configuration = new Array();
+    $scope.configuration['current_exercise'] =  '1';
+
+    $scope.exercises = new Array(
+	{text:'Rhenus fluvius est.', translations:new Array('The Rhine is a river.', 'Rhine is a river.')},
+	{text:'Julia fillia est.', translations:new Array('Julia is a girl.', 'Julia is girl.')});
+
+    $scope.checkTranslation = function() {
+	var currentExercise = $scope.configuration['current_exercise'] - 1;
+	var translations = $scope.exercises[currentExercise].translations
+	for (var i in translations)
+	{
+	    if(translations[i].toLowerCase() == $scope.translation.toLowerCase()){
+		$('#message').css("background-color", "green");
+		$('#message').html('Correct answer.');
+		$scope.nextLesson();
+		return "Ok";
 	    }
-	    $('#message').css("background-color", "red");
-	    $('#message').html('Wrong answer.');
-	    $scope.nextLesson();
-	    return "not OK";
-	};
+	}
+	$('#message').css("background-color", "red");
+	$('#message').html('Wrong answer.');
+	$scope.nextLesson();
+	return "not OK";
+    };
 
-	$scope.nextLesson = function() {
-	    $('#text').val('');
-	    if($scope.exercises.length > $scope.configuration['current_exercise']){
-		$('#message').css("background-color", "white");
-		$scope.configuration['current_exercise']++;
-	    } else {
-		$('#message').html('Session is over.');
-	    }
-	};
+    $scope.nextLesson = function() {
+	$('#text').val('');
+	if($scope.exercises.length > $scope.configuration['current_exercise']){
+	    $('#message').css("background-color", "white");
+	    $scope.configuration['current_exercise']++;
+	} else {
+	    $('#message').html('Session is over.');
+	}
+    };
 
-	$scope.remaining = function() {
-	    var count = 0;
-	    angular.forEach($scope.exercises, function(exercise) {
-		count += exercise.done ? 0 : 1;
-	    });
-	    return count;
-	};
-	
-	$scope.currentLesson = function() {
-	    var count = $scope.configuration['current_exercise'] - 1;
-	    return count;
-	};
+    $scope.remaining = function() {
+	var count = 0;
+	angular.forEach($scope.exercises, function(exercise) {
+	    count += exercise.done ? 0 : 1;
+	});
+	return count;
+    };
+    
+    $scope.currentLesson = function() {
+	var count = $scope.configuration['current_exercise'] - 1;
+	return count;
+    };
 
-	
-	$scope.archive = function() {
-	    var oldTodos = $scope.todos;
-	    $scope.todos = [];
-	    angular.forEach(oldTodos, function(todo) {
-		if (!todo.done) $scope.todos.push(todo);
-	    });
-	};
-    }
+    
+    $scope.archive = function() {
+	var oldTodos = $scope.todos;
+	$scope.todos = [];
+	angular.forEach(oldTodos, function(todo) {
+	    if (!todo.done) $scope.todos.push(todo);
+	});
+    };
+}
 
 
 $(document).ready(function() {
     var lessons = new Array('data/lessons/lesson1.json', 'data/lessons/lesson2.json');
 
-
     var options = {};
-    
+
     $("#babel_window").center();
     $("#babel_window").fadeIn();
-    
+
 
     $( document ).tooltip({
 	position: {
