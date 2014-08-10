492223d963af993ece606508a2eabb5c04bff72d
Henry Hirsch <henry@w3-net.de>
2014-08-10 06:59:45 +0200

    [fix] Licence corrections.

--- js/babel.js
+++ js/babel.js
@@ -1,6 +1,4 @@
 /*
-  All rights reserved 2013 Henry Hirsch
-
   This file is part of Babel.
 
   Babel is free software: you can redistribute it and/or modify
@@ -8,7 +6,7 @@
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
 
-  Foobar is distributed in the hope that it will be useful,
+  Babel is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
